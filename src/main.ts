3c6b7810168841e071f8924d3b8260244dfecf0f
yyjazsf <admin@yingyj.com>
2021-03-30 14:21:25 +0800

    chroe: add dep

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(81);
+  console.log(`application started: http://localhost:81`);
 }
 bootstrap();
