cadfa34043515008ec70af675e3b684fb468724e
yyjazsf <admin@yingyj.com>
2021-04-06 21:56:50 +0800

    dev: ws

---
+++ src/events/events.gateway.ts
@@ -0,0 +1,39 @@
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+  WsResponse,
+} from '@nestjs/websockets';
+import { from, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Server } from 'socket.io';
+
+@WebSocketGateway()
+export class EventsGateway {
+  @WebSocketServer()
+  server: Server;
+
+  onGatewayInit() {
+    console.log('onGatewayInit');
+  }
+  onGatewayConnection() {
+    console.log('onGatewayConnection');
+  }
+
+  onGatewayDisconnect() {
+    console.log('onGatewayDisconnect');
+  }
+
+  @SubscribeMessage('events')
+  findAll(@MessageBody() data: unknown): Observable<WsResponse<number>> {
+    return from([1, 2, 3]).pipe(
+      map((item) => ({ event: 'events', data: item })),
+    );
+  }
+
+  @SubscribeMessage('identity')
+  async identity(@MessageBody() data: number): Promise<number> {
+    return data;
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(81);
   console.log(`application started: http://localhost:81`);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
