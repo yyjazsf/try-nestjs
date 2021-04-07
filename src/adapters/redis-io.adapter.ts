77c30f11de987f4a7fffd5790bd086261f956029
yyjazsf <admin@yingyj.com>
2021-04-07 22:12:32 +0800

    fix: socket client 只兼容 2.x

--- src/adapters/redis-io.adapter.ts
+++ src/adapters/redis-io.adapter.ts
@@ -2,11 +2,11 @@ import { IoAdapter } from '@nestjs/platform-socket.io';
 import { ServerOptions } from 'socket.io';
 import { createAdapter } from 'socket.io-redis';
 
-const redisAdapter = createAdapter({ host: 'localhost', port: 6379 });
-
 export class RedisIoAdapter extends IoAdapter {
   createIOServer(port: number, options?: ServerOptions): any {
     const server = super.createIOServer(port, options);
+    console.log('options RedisIoAdapter', options);
+    const redisAdapter = createAdapter(`localhost:${port}`);
     server.adapter(redisAdapter);
     return server;
   }
